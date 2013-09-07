3cb1b57173b97c6b7006445df3c6ec7bb70eef6f
dolphinsue319 <dolphinsue@mail2000.com.tw>
2013-09-07 10:17:20 +0800

    first commit

---
+++ UIViewCategories/UIView+UIViewToUIImage.h
@@ -0,0 +1,13 @@
+//
+//  UIView+UIViewToUIImage.h
+//  DSShiningLabel
+//
+//  Created by Mac on 13/9/7.
+//  Copyright (c) 2013年 dolphinsue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UIViewToUIImage)
+- (UIImage *)viewToUIImage;
+@end
---
+++ UIViewCategories/UIView+UIViewToUIImage.m
@@ -0,0 +1,21 @@
+//
+//  UIView+UIViewToUIImage.m
+//  DSShiningLabel
+//
+//  Created by Mac on 13/9/7.
+//  Copyright (c) 2013年 dolphinsue. All rights reserved.
+//
+
+#import "UIView+UIViewToUIImage.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation UIView (UIViewToUIImage)
+- (UIImage *)viewToUIImage{
+    UIGraphicsBeginImageContext(self.bounds.size);
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    [self.layer renderInContext:contextRef];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+@end
